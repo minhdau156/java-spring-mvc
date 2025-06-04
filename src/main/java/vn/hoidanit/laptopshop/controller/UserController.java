30ce1f69caabe3e4e695289dd7ce1a01f3eef61f
Nguyen Vo Nghiem Minh <nguyenvonghiemminh156@gmail.com>
2025-06-04 17:18:50 +0700

    Bai 4 Chuong 7

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -10,28 +10,35 @@ import org.springframework.web.bind.annotation.RestController;
 import vn.hoidanit.laptopshop.service.UserService;
 
 
-// @Controller
-// public class UserController {
-    
-//     @RequestMapping("/")
-//     public String getHomePage() {
-//         return "hello from controller";
-//     }
-// }
-
-
-@RestController
+@Controller
 public class UserController {
-    
+
     private UserService userService;
 
     public UserController(UserService userService) {
     this.userService = userService;
     }
-    @GetMapping("")
+    
+    @RequestMapping("/")
     public String getHomePage() {
-        return this.userService.handleHello();
-   
-
+        String test = this.userService.handleHello();
+        return "eric.html";
     }
 }
+
+
+// @RestController
+// public class UserController {
+    
+//     private UserService userService;
+
+//     public UserController(UserService userService) {
+//     this.userService = userService;
+//     }
+//     @GetMapping("")
+//     public String getHomePage() {
+//         return this.userService.handleHello();
+   
+
+//     }
+// }
