75b729954d1de1f8ccf11b4ffab12e4043651f7b
Nguyen Vo Nghiem Minh <nguyenvonghiemminh156@gmail.com>
2025-06-06 19:34:27 +0700

    Bai 53 Chuong 7

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -2,13 +2,12 @@ package vn.hoidanit.laptopshop.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 
+import vn.hoidanit.laptopshop.domain.User;
 import vn.hoidanit.laptopshop.service.UserService;
 
 
@@ -28,6 +27,19 @@ public class UserController {
         model.addAttribute("hoidanit", "from controller with model");
         return "hello";
     }
+
+    @RequestMapping("admin/user") 
+        public String getUserPage(Model model) {
+            String test = this.userService.handleHello();
+            model.addAttribute("newUser", new User());
+            return "/admin/user/create";
+        }
+
+    @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
+    public String createUserPage(Model model, @ModelAttribute("newUser") User hoidanit) {
+            System.out.println("run here" + hoidanit);
+            return "hello";
+        }
 }
 
 
