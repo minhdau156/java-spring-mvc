49f9527535ff4359a21ac3a8f7d66ae6f092f5dd
Nguyen Vo Nghiem Minh <nguyenvonghiemminh156@gmail.com>
2025-06-08 10:58:18 +0700

    Bai 63 Chuong 8

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -33,14 +33,18 @@ public class UserController {
         return "hello";
     }
 
-    @RequestMapping("admin/user") 
-        public String getUserPage(Model model) {
-            String test = this.userService.handleHello();
+    @RequestMapping("admin/user/create") //GET
+        public String getCreateUserPage(Model model) {
             model.addAttribute("newUser", new User());
             return "/admin/user/create";
         }
+    
+    @RequestMapping("admin/user")
+        public String getUserPage(Model model) {
+            return "/admin/user/table-user";
+        }
 
-    @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
+    @RequestMapping(value = "/admin/user/create", method = RequestMethod.POST)
     public String createUserPage(Model model, @ModelAttribute("newUser") User hoidanit) {
             System.out.println("run here " + hoidanit);
             this.userService.handleSaveUser(hoidanit);
