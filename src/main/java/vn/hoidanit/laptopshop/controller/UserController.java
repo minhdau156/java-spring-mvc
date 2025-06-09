90102353bd08056520275bfd3f215c88f1e7130a
Nguyen Vo Nghiem Minh <nguyenvonghiemminh156@gmail.com>
2025-06-09 10:05:11 +0700

    Bai 65 Chuong 8

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -39,11 +40,18 @@ public class UserController {
             return "/admin/user/create";
         }
     
-    @RequestMapping("admin/user")
+    @RequestMapping("/admin/user")
         public String getUserPage(Model model) {
             List<User> users = this.userService.getAllUsers();
             model.addAttribute("users1", users);
             return "/admin/user/table-user";
+        }
+
+    @RequestMapping("/admin/user/{hoidanit}")
+        public String getUserDetailPage(Model model, @PathVariable long id) {
+            System.out.println("check path id " + id);
+            model.addAttribute(\"id", id);
+            return "/admin/user/show";
         }
 
     @RequestMapping(value = "/admin/user/create", method = RequestMethod.POST)
