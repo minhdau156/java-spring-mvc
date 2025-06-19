1b06aa006ee00054b9cb1912f73ff53b98597ce2
Nguyen Vo Nghiem Minh <nguyenvonghiemminh156@gmail.com>
2025-06-19 11:03:09 +0700

    Bai 105 Chuong 12

--- src/main/java/vn/hoidanit/laptopshop/domain/Product.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/Product.java
@@ -2,6 +2,7 @@ package vn.hoidanit.laptopshop.domain;
 
 
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -31,7 +32,9 @@ public class Product {
 
     @NotNull
     @NotEmpty(message = "detailDesc không được để trống")
+    @Column(columnDefinition = "MEDIUMTEXT")
     private String detailDesc;
+
 
     @NotNull
     @NotEmpty(message = "shortDesc không được để trống")
