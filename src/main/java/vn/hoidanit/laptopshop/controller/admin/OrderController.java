2e5ed124b23aa12870cbcb3081ec4057755522aa
Nguyen Vo Nghiem Minh <nguyenvonghiemminh156@gmail.com>
2025-06-26 20:05:27 +0700

    Bai 133 Chuong 14

--- src/main/java/vn/hoidanit/laptopshop/controller/admin/OrderController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/admin/OrderController.java
@@ -1,12 +1,66 @@
 package vn.hoidanit.laptopshop.controller.admin;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import vn.hoidanit.laptopshop.domain.Order;
+import vn.hoidanit.laptopshop.domain.OrderDetail;
+import vn.hoidanit.laptopshop.repository.OrderRepository;
+import vn.hoidanit.laptopshop.service.OrderService;
 
 @Controller
 public class OrderController {
-    @GetMapping("admin/order")
-    public String getDashBoard() {
+    private final OrderService orderService;
+    
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping("/admin/order")
+    public String getDashBoard(Model model) {
+        List<Order> orders = this.orderService.fetchAllOrders();
+        model.addAttribute("orders", orders);
         return "admin/order/show";
     }
+
+    @GetMapping("/admin/order/{id}")
+    public String getDetailOrderPage(Model model, @PathVariable long id) {
+        Order currentOrder = this.orderService.fetchOrderById(id).get();
+        model.addAttribute("order", currentOrder);
+        model.addAttribute("id", id);
+        model.addAttribute("orderDetails", currentOrder.getOrderDetails());
+        return "admin/order/detail";
+    }
+
+    @GetMapping("/admin/order/delete/{id}")
+    public String getDeleteOrderPage(Model model, @PathVariable long id) {
+        model.addAttribute("id", id);
+        model.addAttribute("newOrder", new Order());
+        return "admin/order/delete";
+    }
+    @PostMapping("/admin/order/delete")
+    public String postDeleteOrder(@ModelAttribute("newOrder") Order order) {
+        this.orderService.deleteOrderById(order.getId());
+        return "redirect:/admin/order";
+    }
+
+    @GetMapping("/admin/order/update/{id}")
+    public String getUpdateOrderPage(Model model, @PathVariable long id) {
+        Optional<Order> currentOrder = this.orderService.fetchOrderById(id);
+        model.addAttribute("newOrder", currentOrder.get());
+        return "admin/order/update";
+    }
+
+    @PostMapping("/admin/order/update")
+    public String postUpdateOrder(@ModelAttribute("newOrder") Order order) {
+        this.orderService.updateOrder(order);
+        return "redirect:/admin/order";
+    }   
 }
--- src/main/java/vn/hoidanit/laptopshop/repository/OrderRepository.java
+++ src/main/java/vn/hoidanit/laptopshop/repository/OrderRepository.java
@@ -1,5 +1,7 @@
 package vn.hoidanit.laptopshop.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,6 @@ import vn.hoidanit.laptopshop.domain.Order;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
-    
+    List<Order> findAll();
+    Order findById(long id);
 }
