a9d9c62203be8c6b493ff39ac2d2ddbefb89d945
Nguyen Vo Nghiem Minh <nguyenvonghiemminh156@gmail.com>
2025-06-27 10:26:36 +0700

    Bai 144 Chuong 15

--- src/main/java/vn/hoidanit/laptopshop/controller/client/ItemController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/client/ItemController.java
@@ -2,7 +2,11 @@ package vn.hoidanit.laptopshop.controller.client;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,8 +31,34 @@ public class ItemController {
         this.productService = productService;
     }
 
+    @GetMapping("/products")
+    public String getProductPage(Model model, @RequestParam("page") Optional<String> pageOptional) {
+        int page = 1;
+        try {
+            if (pageOptional.isPresent()) {
+                //convert from string to int
+                page = Integer.parseInt(pageOptional.get());
+            }
+            else {
+
+            }
+        }
+        catch (Exception e) {
+
+        }
+
+        Pageable pageable = PageRequest.of(page - 1, 6);
+        Page<Product> products = this.productService.fetchProducts(pageable);
+        List<Product> listProducts = products.getContent();
+        model.addAttribute("products", listProducts);
+        model.addAttribute("currentPage", page);
+        model.addAttribute("totalPages", products.getTotalPages());
+        return "client/product/show";
+    }
+
+
     @GetMapping("/product/{id}")
-    public String getProductPage(Model model, @PathVariable long id) {
+    public String getProductDetailPage(Model model, @PathVariable long id) {
         Product pr = this.productService.fetchProductById(id).get();
         model.addAttribute("product", pr);
         model.addAttribute("id", id);
